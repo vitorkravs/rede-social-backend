e4976edcbe978801a333cc41c0b268744a21be19
vitor kravszenko <vitorkravs3@gmail.com>
2024-02-17 18:35:56 -0300

    feat: se conectando ao banco de dados mysql

---
+++ src/connect.js
@@ -0,0 +1,11 @@
+import mysql from "mysql";
+import { config } from "dotenv";
+
+config();
+
+export const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB,
+});
